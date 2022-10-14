4d016d862feb331b03f208912cdf55332efb816f
dipanshu <medipansh@gmail.com>
2022-10-14 08:17:49 +0530

    cleanup

---
+++ pages/1.js
@@ -0,0 +1,22 @@
+export default () => {
+    return(
+      <p>
+      I'm 2nd year student perusing B-TECH in Computer science and Engineering.
+      I found about this program through the open source community. I love open
+      source. I think that as developers, we should band together to support the
+      expansion and prosperity of the open source community. It will assist me
+      in closing the gap between learning and truly implementing what we have
+      learnt. It will provide me the opportunity to tackle problems in the real
+      world. I've always believed in "learning by doing". It facilitated the
+      growth of strong research skills. I now know how to handle the challenges
+      that developers face. It enhanced my ability to solve problems. As a
+      self-taught programmer, I was able to acquire a strong commitment and
+      passion. I'm a full stack developer. I have great foundation in some of
+      programming languages like javascript, java, python and go. I also been
+      continuously been learning the cloud native technology like docker,
+      kubernetes, service mesh. I hope to learn how to be responsible as
+      developer. The LFX mentorship will also empower me to hone my abilities
+      and lay a solid platform for my future contributions to open source.
+    </p>
+    )
+}
